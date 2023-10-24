d18fb5c85c15efb532574b9bde7ab4bd1869ddad
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-24 13:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function removeItem(){
+	const color = document.getElementById("colorSelect");
+	const presentColor = color.value;
+	const tag = document.getElementByTagName("input");
+
+	         if(color === presentColor)
+			 {
+				 console.log(tag);
+			 }
+	else{
+		console.log("");
+	}
+}
+removeItem();
